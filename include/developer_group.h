936b405fca5d7602eafecb978eafb0e1bcd3434c
Clarence Pereira <86191768+clpe2@users.noreply.github.com>
2025-03-16 15:40:33 +0100

    Update developer_group.h
    Prototypes for logo and group. Struct containing logo and developers to output "group". ver. 1.0

--- include/developer_group.h
+++ include/developer_group.h
@@ -1,1 +1,21 @@
-git
+#ifndef DEVELOPER_GROUP_H
+#define DEVELOPER_GROUP_H
+
+#include "developer.h"
+
+#define MAX_DEVELOPERS 2
+#define LOGO_WIDTH 40
+#define LOGO_HEIGHT 20
+
+typedef struct {
+    Developer developers[MAX_DEVELOPERS];
+    char logo[LOGO_HEIGHT][LOGO_WIDTH + 1]; // +1 für Nullterminierung
+    int num_developers;
+} DeveloperGroup;
+
+DeveloperGroup create_developer_group(const char logo[LOGO_HEIGHT][LOGO_WIDTH + 1]);
+void add_developer(DeveloperGroup *group, Developer developer);
+void print_group_logo(DeveloperGroup *group);
+void print_group(DeveloperGroup *group);
+
+#endif // DEVELOPER_GROUP_H
