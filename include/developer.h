83821f7110ad540f6ca2f30ae9bd0e9ef5bfae1b
Clarence Pereira <86191768+clpe2@users.noreply.github.com>
2025-03-20 23:42:54 +0100

    Update developer.h
    Added comments to the file

--- include/developer.h
+++ include/developer.h
@@ -1,14 +1,14 @@
 #ifndef DEVELOPER_H
 #define DEVELOPER_H
 
-#include <stdio.h>
+#include <stdio.h>  //including the stdio.h library
 
 typedef struct {
     char name[50];
     char alias[50];
-} Developer;
+} Developer;  //struct to define the parameters of the Developer
 
-Developer create_developer(const char *name, const char *alias);
-void print_developer(Developer *developer);
+Developer create_developer(const char *name, const char *alias); //functions to access and specify the variables in the struct Developer
+void print_developer(Developer *developer); //functions to access and specify the variables in the struct Developer
 
 #endif // DEVELOPER_H
