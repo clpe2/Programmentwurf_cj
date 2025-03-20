45c97ddf0b55225bbaa141000357cff915d41f09
Clarence Pereira <86191768+clpe2@users.noreply.github.com>
2025-03-20 23:48:43 +0100

    Update developer.c
    comments in the file

--- src/developer.c
+++ src/developer.c
@@ -1,13 +1,13 @@
-#include "developer.h"
-#include <string.h>
+#include "developer.h" // including the developer.h library in order to define its contents within the .c file
+#include <string.h> //using functions of the string.h library
 
-Developer create_developer(const char *name, const char *alias) {
-    Developer dev;
-    strcpy(dev.name, name);
-    strcpy(dev.alias, alias);
-    return dev;
+Developer create_developer(const char *name, const char *alias) { //input variables of the create_developer function that uses the struct Developer
+    Developer dev; // creating a "object"/ struct called dev
+    strcpy(dev.name, name); //copying the input name into the dev variable name
+    strcpy(dev.alias, alias); //copying the input alias into the dev variable alias
+    return dev; // the return of the function is the dev that now has its name array and char array filled with contents
 }
 
 void print_developer(Developer *developer) {
     printf("Name: %s, Alias: %s\n", developer->name, developer->alias);
-}
+} //output of the developer information specified through create_developer via the variable dev
