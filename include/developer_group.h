8a8c9b415bd9066c03a98fbc73638a23dd3927f4
Clarence Pereira <86191768+clpe2@users.noreply.github.com>
2025-03-20 23:40:57 +0100

    Update developer_group.h
    Updated the file with comments

--- include/developer_group.h
+++ include/developer_group.h
@@ -1,21 +1,24 @@
 #ifndef DEVELOPER_GROUP_H
 #define DEVELOPER_GROUP_H
 
-#include "developer.h"
+#include "developer.h"    //including the developer.h library to use in and build developer_group.h
 
-#define MAX_DEVELOPERS 2
-#define LOGO_WIDTH 155
-#define LOGO_HEIGHT 59
+#define MAX_DEVELOPERS 2  
+#define LOGO_WIDTH 155    
+#define LOGO_HEIGHT 59    
+//Maximum developer count
+//group logo specifications width
+//group logo specifications height
 
 typedef struct {
     Developer developers[MAX_DEVELOPERS];
-    char logo[LOGO_HEIGHT][LOGO_WIDTH + 1]; // +1 für null termination
+    char logo[LOGO_HEIGHT][LOGO_WIDTH + 1]; // +1 NULL TERMINATOR
     int num_developers;
-} DeveloperGroup;
+} DeveloperGroup;  // prototype struct to define the parameters of the "DeveloperGroup"
 
-DeveloperGroup create_developer_group(const char logo[LOGO_HEIGHT][LOGO_WIDTH + 1]);
-void add_developer(DeveloperGroup *group, Developer developer);
-void print_group_logo(DeveloperGroup *group);
-void print_group(DeveloperGroup *group);
+DeveloperGroup create_developer_group(const char logo[LOGO_HEIGHT][LOGO_WIDTH + 1]); //functions that define the different variables in the struct DeveloperGroup
+void add_developer(DeveloperGroup *group, Developer developer); //functions that define the different variables in the struct DeveloperGroup
+void print_group_logo(DeveloperGroup *group); //functions that define the different variables in the struct DeveloperGroup
+void print_group(DeveloperGroup *group); //functions that define the different variables in the struct DeveloperGroup
 
 #endif // DEVELOPER_GROUP_H
